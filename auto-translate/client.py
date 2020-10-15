f216640383c92e043163a7a96fd9a4ca8a781f7b
Thirdwater <irinthirdwater@gmail.com>
2020-10-15 16:42:26 +0100

    Detect guilds and text channels

--- auto-translate/client.py
+++ auto-translate/client.py
@@ -2,6 +2,14 @@ import discord
 
 class AutoTranslateClient(discord.Client):
 
+    def __init__(self):
+        discord.Client.__init__(self)
+        print("Initialising AutoTranslateClient...")
+
     async def on_ready(self):
         print(f"{self.user} has connected to Discord!")
+        for guild in self.guilds:
+            print(f"  Guid: {guild.id}\t{guild.name}")
+            for text_channel in guild.text_channels:
+                print(f"    Channel: {text_channel.id}\t{text_channel.name}")
 
