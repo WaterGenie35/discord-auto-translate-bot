cc6e676d0392e4173c4c4c92e5021f4d1dc86c39
Thirdwater <irinthirdwater@gmail.com>
2020-10-16 09:51:11 +0100

    Add auto-translation on test channels

--- auto-translate/client.py
+++ auto-translate/client.py
@@ -19,3 +19,28 @@ class AutoTranslateClient(discord.Client):
         print(self.service.translate(
                 Language.ENGLISH, Language.RUSSIAN, "hello"))
 
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        
+        print(f"Received message from {message.author} in {message.channel.name}.")
+        guild = self.get_guild(765773043569393674)
+        ru_channel = guild.get_channel(766287213888798730)
+        en_channel = guild.get_channel(765806897558716446)
+        translation = None
+        target_channel = None
+        if message.channel == ru_channel:
+            translation = self.service.translate(
+                    Language.RUSSIAN, Language.ENGLISH, message.content)
+            target_channel = en_channel
+        elif message.channel == en_channel:
+            translation = self.service.translate(
+                    Language.ENGLISH, Language.RUSSIAN, message.content)
+            target_channel = ru_channel
+
+        if target_channel:
+            print(f"Sending translation to {target_channel.name}...")
+            translation = f"**{message.author.display_name}:** " + translation
+            await target_channel.send(translation)
+            
+
